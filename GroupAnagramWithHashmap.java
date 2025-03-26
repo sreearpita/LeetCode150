0bf20f5bedaa411457d3f902f3b3837622de591f
sreearpita <47421921+sreearpita@users.noreply.github.com>
2025-03-26 19:06:35 +0800

    Update and rename GroupAnagramWithHashmap to GroupAnagramWithHashmap.java

--- GroupAnagramWithHashmap.java
+++ GroupAnagramWithHashmap.java
@@ -1,3 +1,25 @@
+/* Problem Statement:
+Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
+An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+Example 1:
+Input: strs = ["act","pots","tops","cat","stop","hat"]
+Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+
+Example 2:
+Input: strs = ["x"]
+Output: [["x"]]
+
+Example 3:
+Input: strs = [""]
+utput: [[""]]
+
+Constraints:
+    1 <= strs.length <= 1000.
+    0 <= strs[i].length <= 100
+    strs[i] is made up of lowercase English letters.
+
+*/
 public class Group_anagramusingHashmap {
 	public List<List<String>> groupAnagrams(String[] strs) {
         if (strs == null || strs.length == 0) return new ArrayList<>();
